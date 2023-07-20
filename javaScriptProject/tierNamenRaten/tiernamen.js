5725c67bd41f2ae58f397aa47410813f9de206c6
= <=>
2023-07-20 13:23:34 +0200

    Finishing the namenraten program

--- javaScriptProject/tierNamenRaten/tiernamen.js
+++ javaScriptProject/tierNamenRaten/tiernamen.js
@@ -1,25 +1,43 @@
 const prompt = require('prompt-sync')({ sigint: true });
-
-const tierliste = ["Elephant", "Delphin", "Fuchs", "Dachs", "Gans", "Eierlegendewollmilchsau"]
-
+const tierliste = ["Elephant", "Delphin", "Fuchs", "Dacahs", "Gans", "Eierlegendewollmilchsau"]
 let tiername = RandomName().toUpperCase()
-
-for (i = tiername.length + 7; i > 0; i--) {
-    input = prompt('Gib ein Buchstaben ein').toUpperCase()
-    let hashedName = tiername.replace(/./gi, '.')
-    if (tiername.includes(input)) {
-        index = GetCharIndex(input, tiername)
-        hashedName = tiername.substring(0, index) + input + tiername.substring(index + 1);
-        console.log(hashedName)
+let hashedName = tiername.replace(/./gi, '.')
+index = []
+const alphabeticPattern = /^[A-Za-z]+$/;
+for (let i = tiername.length + 7; i > 0; i--) {
+    console.log(`Du Hast Noch ${i} Versuch\n`, hashedName, '\n')
+    input = prompt('Gib ein Buchstaben ein: ').toUpperCase()
+    if (input.length === 1 & input.trim() !== '' & alphabeticPattern.test(input)) {
+        if (tiername.includes(input)) {
+            index = GetCharIndexs(input, tiername)
+            for (x of index) {
+                hashedName = hashedName.substring(0, x) + input + hashedName.substring(x + 1);
+            }
+            i++
+        }
+        if (!hashedName.includes('.')) {
+            console.log('\nRichtige Antwort: ', hashedName, '\nBRAVO TEZE\n')
+            break
+        }
+        else if (i === 1){
+            console.log(`\nDein Antwort: ${hashedName}\nRichtige Antwort: ${tiername}\n`)
+        }
+    }
+    else {
+        console.log('Falsche Eingabe')
+        i++
     }
 }
-
 function RandomName() {
     nameIndex = Math.floor(Math.random() * tierliste.length)
     return tierliste[nameIndex]
 }
-
-function GetCharIndex(input, tiername) {
-    index = tiername.indexOf(input)
-    return index
+function GetCharIndexs(input, tiername) {
+    const index = [];
+    for (let i = 0; i < tiername.length; i++) {
+        if (tiername[i] === input) {
+            index.push(i);
+        }
+    }
+    return index;
 }
