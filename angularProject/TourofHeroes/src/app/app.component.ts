a8012c7a4ff1c0d9989ea681bd5618dad3b61480
Samooooooo <osama.thabit1@gmail.com>
2023-07-25 09:02:39 +0200

    border

--- angularProject/TourofHeroes/src/app/app.component.ts
+++ angularProject/TourofHeroes/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
+  showHeroesLink = true;
+
+  hideHeroesLink() {
+    this.showHeroesLink = false;
+  }
 }
