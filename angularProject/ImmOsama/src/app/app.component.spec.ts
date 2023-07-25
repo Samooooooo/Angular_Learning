b50e82838ed9f94077cd42eb84bded40384e9833
Samooooooo <osama.thabit1@gmail.com>
2023-07-25 13:41:49 +0200

    .

--- angularProject/ImmOsama/src/app/app.component.spec.ts
+++ angularProject/ImmOsama/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ImmoSosama'`, () => {
+  it(`should have as title 'ImmOsama'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ImmoSosama');
+    expect(app.title).toEqual('ImmOsama');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ImmoSosama app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('ImmOsama app is running!');
   });
 });
