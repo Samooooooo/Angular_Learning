1edf49f4f8890dec219ae52ea954efefb67e9712
Samooooooo <osama.thabit1@gmail.com>
2023-08-21 20:51:51 +0200

    ....

--- angularProject/LPC1-APP/LLCE-App/src/app/shared/score/score.service.ts
+++ angularProject/LPC1-APP/LLCE-App/src/app/shared/score/score.service.ts
@@ -13,6 +13,7 @@ export class ScoreService {
     remaining: 120,
     wrongQuestions: [],
     skipedQuestions: [],
+    rightQuestions: [],
     questionCounter: 0,
   };
 
@@ -30,54 +31,61 @@ export class ScoreService {
     const updatedScores = { ...this.scores };
     if (Array.isArray(answers)) {
       const correctAnswersFirstChars = question.correctAnswer.map((correct) =>
-        correct.charAt(0),
+        correct.charAt(0)
       );
       const isCorrect = answers.every((answer) =>
-        correctAnswersFirstChars.includes(answer.charAt(0)),
+        correctAnswersFirstChars.includes(answer.charAt(0))
       );
       if (isCorrect && answers.length === question.correctAnswer.length) {
         updatedScores.correct++;
         updatedScores.remaining--;
-      } else {
+        if (!this.scores.rightQuestions.some((q=>q.index === question.index))) {
+          updatedScores.rightQuestions.push(question)
+        }
         if (question.skipped) {
-          if (
-            !this.scores.skipedQuestions.some((q) => q.index === question.index)
-          ) {
-            updatedScores.skipped++;
-            updatedScores.skipedQuestions.push(question);
-          }
+          updatedScores.skipped--;
         } else {
-          if (Array.isArray(answers) && answers.length !== 0) {
-            if (
-              !this.scores.wrongQuestions.includes(question) &&
-              !this.scores.wrongQuestions.some(
-                (q) => q.selectedAnswer[0] === question.selectedAnswer[0],
-              )
-            ) {
-              updatedScores.wrong++;
-              updatedScores.wrongQuestions.push(question);
-            }
+          updatedScores.skipedQuestions = updatedScores.skipedQuestions.filter(
+            (q) => q.index !== question.index
+          );
+        }
+      } else {
+        if (question.skipped && !this.scores.skipedQuestions.some((q) => q.index === question.index)) {
+          updatedScores.skipped++;
+          updatedScores.skipedQuestions.push(question);
+        } else if (Array.isArray(answers) && answers.length !== 0) {
+          if (
+            !this.scores.wrongQuestions.includes(question) &&
+            !this.scores.wrongQuestions.some(
+              (q) => q.selectedAnswer[0] === question.selectedAnswer[0]
+            )
+          ) {
+            updatedScores.wrong++;
+            updatedScores.wrongQuestions.push(question);
           }
         }
       }
     } else {
-      //Fill IN
       if (answers.includes(question.correctAnswer[0])) {
         updatedScores.correct++;
         updatedScores.remaining--;
+        updatedScores.skipedQuestions = updatedScores.skipedQuestions.filter(
+          (q) => q.index !== question.index
+        );
+        updatedScores.skipped--;
       } else {
-        if (question.skipped) {
-          if (!updatedScores.skipedQuestions.includes(question)) {
-            updatedScores.skipped++;
-            updatedScores.skipedQuestions.push(question);
-          }
+        if (question.skipped && !updatedScores.skipedQuestions.includes(question)) {
+          updatedScores.skipped++;
+          updatedScores.skipedQuestions.push(question);
         } else {
           updatedScores.wrong++;
           updatedScores.wrongQuestions.push(question);
         }
       }
     }
-    updatedScores.questionCounter++;
+    updatedScores.questionCounter = updatedScores.rightQuestions.length
+    updatedScores.questionCounter = updatedScores.rightQuestions.length
+    updatedScores.questionCounter = updatedScores.rightQuestions.length
     this.updateScores(updatedScores);
   }
 }
